171a05a16ad40af67162e2ac31e628238829230f
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-28 12:53:23 +0200

    Create DemoApplication.java

---
+++ src/main/java/com/iag/swe/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.iag.swe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* DemoApplication is application type "hello world", 
+* this print to message on browser. 
+**/
+@SpringBootApplication
+public class DemoApplication {
+	/**
+	 *  main appication start with spring framework library SpringApplication.run()
+	 **/
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
