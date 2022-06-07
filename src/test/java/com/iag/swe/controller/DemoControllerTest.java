2099dd2c4ffafe99267cd01854158abf4dfc2418
NestorRescigno <nestor.rescigno@accenture.com>
2022-06-07 08:31:48 +0200

    include new test

---
+++ src/test/java/com/iag/swe/controller/DemoControllerTest.java
@@ -0,0 +1,25 @@
+package com.iag.swe.controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+* this is the controller class
+*/
+@SpringBootTest
+public class DemoControllerTest {
+	/**
+	*	Give unit test dummy 
+	*	when this is running 
+	*	then return true.
+	*/
+	@Test
+	void helloTest() {
+		DemoController demoTest = new DemoController();
+		 assertTrue(demoTest.equals("hello world!!!"),"the unit test dummy is running and ok ;)");
+	}
+
+
+}
+
